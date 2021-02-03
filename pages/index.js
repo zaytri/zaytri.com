ca7fac2cf16ff8031bf3b5507262de5054a2ce9c
Zaytri <9072119+zaytri@users.noreply.github.com>
2021-02-03 01:20:31 -0800

    Initial Design

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,48 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Zaytri</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
+      <div className={styles.spotlight}>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.message}>
+          <p>You Have Entered</p>
+          <p>Zaytri Space</p>
+        </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+        <div className={styles.social}>
+          <a href='https://www.instagram.com/imzaytri/'>
+            <img src='/circle-instagram.png' alt='Instagram Link' />
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href='https://www.youtube.com/channel/UCIp9bzUH8ylhSZ7e8-Gc_Cg'>
+            <img src='/circle-youtube.png' alt='YouTube Link' />
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href='https://www.twitch.tv/zaytri'>
+            <img src='/circle-twitch.png' alt='Twitch Link' />
           </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href='https://twitter.com/imzaytri'>
+            <img src='/circle-twitter.png' alt='Twitter Link' />
+          </a>
+          <a href='https://www.tiktok.com/@imzaytri'>
+            <img src='/circle-tiktok.png' alt='TikTok Link' />
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          <img src='/screme.png' alt='Screaming Toad' className={styles.toad} />
       </footer>
     </div>
   )
