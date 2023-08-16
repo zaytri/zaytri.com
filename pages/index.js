772011253ab809a9b0bd6032fbb189525ac8c2b9
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-08-16 01:34:03 -0700

    Added Palia Crop Boost Visualizer link

--- pages/index.js
+++ pages/index.js
@@ -53,6 +53,12 @@ export default function Home() {
 
           <SocialButton
             classes={[styles.widget]}
+            url='https://docs.google.com/spreadsheets/d/1z60kAOtJTESQocGVLz33tv1u2Y9jilwtNU6f67Ww42g/edit#gid=0'
+            label='Palia Crop Boost Visualizer'
+          />
+
+          <SocialButton
+            classes={[styles.widget]}
             url='https://streamelements.com/dashboard/overlays/share/60d90ef30fcb3e75ea529f37'
             label='Chat Bubbles'
           />
